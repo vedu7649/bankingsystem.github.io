39fb501d2fdffe788cd44d1aae9c9bc7b85823a5
vedu__sutar <103143204+vedu7649@users.noreply.github.com>
2022-09-14 21:49:24 +0530

    Update my_bank.sql

--- basicbankingsystem/my_bank.sql
+++ basicbankingsystem/my_bank.sql
@@ -40,16 +40,16 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`sno`, `sender`, `receiver`, `balance`, `datetime`) VALUES
-(1, 'Saneet', 'Ankit', 200, '2021-04-17 18:58:27'),
-(2, 'Saneet', 'Ankit', 200, '2021-04-17 19:00:31'),
-(3, 'Ankit', 'Niky', 5000, '2021-04-17 19:01:32'),
-(4, 'Saneet', 'Niky', 5000, '2021-04-17 19:06:15'),
-(5, 'Arjun', 'Ankit', 500, '2021-04-17 19:28:24'),
-(6, 'Arjun', 'Ankit', 500, '2021-04-17 19:30:31'),
-(7, 'Arjun', 'Ankit', 200, '2021-04-17 19:31:22'),
-(8, 'Siddhu', 'Aman', 520, '2021-04-18 07:39:58'),
-(9, 'Arka', 'Saneet', 500, '2021-04-18 09:37:08'),
-(10, 'Saneet', 'Aman', 100, '2021-04-18 11:52:18');
+(1, 'Aditya', 'Pouras', 200, '2021-04-17 18:58:27'),
+(2, 'Prem', 'Vedant', 200, '2021-04-17 19:00:31'),
+(3, 'Vedant', 'Chinmay', 5000, '2021-04-17 19:01:32'),
+(4, 'Dhiraj', 'Shravani', 5000, '2021-04-17 19:06:15'),
+(5, 'Pouras', 'Aditya', 500, '2021-04-17 19:28:24'),
+(6, 'Chinmay', 'Pouras', 500, '2021-04-17 19:30:31'),
+(7, 'Tanisha', 'Vedant', 200, '2021-04-17 19:31:22'),
+(8, 'Shravani', 'Vedant', 520, '2021-04-18 07:39:58'),
+(9, 'Vicky', 'Shravni', 500, '2021-04-18 09:37:08'),
+(10, 'Pranav', 'Vicky', 100, '2021-04-18 11:52:18');
 
 -- --------------------------------------------------------
 
@@ -69,24 +69,19 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `name`, `email`, `balance`) VALUES
-(1087, 'Ankit', 'shah@gmail.com', 46600),
-(2654, 'Aman', 'chuha@gmail.com', 30620),
-(3624, 'Gaurav', 'mallu@gmail.com', 40000),
-(4542, 'Arjun', 'nepali@gmail.com', 48800),
-(5345, 'Saneet', 'zombie@gmail.com', 35000),
-(5465, 'Arka', 'baby@gmail.com', 49500),
-(6554, 'Niky', 'loveday@gmail.com', 40000),
-(7875, 'Siddhu', 'pajji@gmail.com', 49480),
-(8877, 'Suvam', 'perimal@gmail.com', 40000),
-(9245, 'Sachin', 'nalla@gmail.com', 30000);
+(1087, 'Aditya', 'adi@gmail.com', 46600),
+(2654, 'Prem', 'prem@gmail.com', 30620),
+(3624, 'Vedant', 'ved@gmail.com', 40000),
+(4542, 'Dhiraj', 'dhi@gmail.com', 48800),
+(5345, 'Pouras', 'pou@gmail.com', 35000),
+(5465, 'Chinmay', 'chi@gmail.com', 49500),
+(6554, 'Tanisha', 'tan@gmail.com', 40000),
+(7875, 'Shravni', 'shr@gmail.com', 49480),
+(8877, 'Vicky', 'vic@gmail.com', 40000),
+(9245, 'Pranav', 'pra@gmail.com', 30000);
 
 --
--- Indexes for dumped tables
---
 
---
--- Indexes for table `transaction`
---
 ALTER TABLE `transaction`
   ADD PRIMARY KEY (`sno`);
 
